61657028a6ff323788a0ca9f5321fb06633d24cf
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-10 16:12:13 -0700

    icons added for header ;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { Button } from '../components/ui/button'
+import Image from 'next/image'
 
 function Header() {
     return (
@@ -10,11 +11,18 @@ function Header() {
                     <span className="text-xl font-bold text-blue-500">Carstairs Kids&apos; Contest</span>
                 </Link>
                 <nav className="flex items-center space-x-4">
-                    <Link href="/rules">
+                    <Link href="/rules" >
                         <Button
                             variant="outline"
                             className="font-medium border-2 border-blue-400 text-blue-600 hover:bg-blue-100"
                         >
+                            <Image
+                                src="/checklist.png"
+                                alt="Checklist Icon"
+                                width={24}
+                                height={24}
+                                className="inline-block pr-1 blue-filter"
+                            />
                             Rules
                         </Button>
                     </Link>
@@ -23,6 +31,13 @@ function Header() {
                             variant="outline"
                             className="font-medium border-2 border-green-400 text-green-600 hover:bg-green-100"
                         >
+                            <Image
+                                src="/maps-and-flags.png"
+                                alt="Checklist Icon"
+                                width={24}
+                                height={24}
+                                className="inline-block pr-1 green-filter"
+                            />
                             Venue Info
                         </Button>
                     </Link>
